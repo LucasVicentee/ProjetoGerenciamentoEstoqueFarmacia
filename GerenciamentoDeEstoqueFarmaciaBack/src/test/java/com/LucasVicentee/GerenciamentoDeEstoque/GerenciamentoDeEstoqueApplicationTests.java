c2b5b89b28350daff4491eb27b7e416272baee6b
LucasVicentee <lh201612@gmail.com>
2025-08-27 10:51:38 -0300

    Inicializando o projeto

---
+++ GerenciamentoDeEstoqueFarmaciaBack/src/test/java/com/LucasVicentee/GerenciamentoDeEstoque/GerenciamentoDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LucasVicentee.GerenciamentoDeEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
