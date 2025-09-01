01ec5b81a22100835de662d50ad3ae6c5a167199
LucasVicentee <lh201612@gmail.com>
2025-09-01 19:42:01 -0300

    Adicionando a classe UsuarioResponseDTO para maior segurana da senha

--- GerenciamentoDeEstoqueFarmaciaBack/src/main/java/com/LucasVicentee/GerenciamentoDeEstoque/dto/UsuarioResponseDTO.java
+++ GerenciamentoDeEstoqueFarmaciaBack/src/main/java/com/LucasVicentee/GerenciamentoDeEstoque/dto/UsuarioResponseDTO.java
@@ -2,5 +2,41 @@ package com.LucasVicentee.GerenciamentoDeEstoque.dto;
 
 public class UsuarioResponseDTO {
 
+    private Long id;
+    private String usuario;
+    private String email;
 
+    public UsuarioResponseDTO() {
+
+    }
+
+    public UsuarioResponseDTO(Long id, String usuario, String email) {
+        this.id = id;
+        this.usuario = usuario;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
